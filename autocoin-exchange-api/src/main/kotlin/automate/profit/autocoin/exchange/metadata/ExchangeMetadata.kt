1024a35a127985a8e856326b0aa95c06bb7d211b
Mikołaj Grząślewicz <mikolaj.grzaslewicz@gmail.com>
2019-02-17 22:04:21 +0100

    Add maximumAmount to metadata

--- autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/metadata/ExchangeMetadata.kt
+++ autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/metadata/ExchangeMetadata.kt
@@ -3,9 +3,15 @@ package automate.profit.autocoin.exchange.metadata
 import automate.profit.autocoin.exchange.currency.CurrencyPair
 import java.math.BigDecimal
 
+/**
+ * TODO
+ * Implement mechanism for providing exchange metadata including cases like
+ * BinanceCurrencyPairMetaData -> scale = currencyPairMetadata.minimumAmount.stripTrailingZeros().scale()
+ */
 data class CurrencyPairMetadata(
         val scale: Int,
-        val minimumAmount: BigDecimal
+        val minimumAmount: BigDecimal,
+        val maximumAmount: BigDecimal
 )
 
 data class CurrencyMetadata(
