57c398af71919a6f3db3a8ee03602a058141494a
Mikołaj Grząślewicz <mikolaj.grzaslewicz@gmail.com>
2019-03-10 11:58:14 +0100

    Add getCurrencyBalance to ExchangeWalletService

--- autocoin-exchange-api-xchange/src/main/kotlin/automate/profit/autocoin/exchange/wallet/XchangeExchangeWalletService.kt
+++ autocoin-exchange-api-xchange/src/main/kotlin/automate/profit/autocoin/exchange/wallet/XchangeExchangeWalletService.kt
@@ -2,6 +2,7 @@ package automate.profit.autocoin.exchange.wallet
 
 import automate.profit.autocoin.exchange.apikey.ExchangeKeyService
 import automate.profit.autocoin.exchange.apikey.ExchangeService
+import automate.profit.autocoin.exchange.currency.CurrencyBalance
 import automate.profit.autocoin.exchange.peruser.UserExchangeServicesFactory
 import automate.profit.autocoin.exchange.peruser.UserExchangeWalletService
 import java.math.BigDecimal
@@ -9,9 +10,9 @@ import java.math.BigDecimal
 class XchangeExchangeWalletService(private val exchangeService: ExchangeService,
                                    private val exchangeKeyService: ExchangeKeyService,
                                    private val userExchangeServicesFactory: UserExchangeServicesFactory) : ExchangeWalletService {
-    override fun getAvailableAmountOfCurrency(exchangeName: String, exchangeUserId: String, currencyCode: String): BigDecimal {
+    override fun getCurrencyBalance(exchangeName: String, exchangeUserId: String, currencyCode: String): CurrencyBalance {
         val userExchangeWalletService = getUserExchangeWalletService(exchangeName, exchangeUserId)
-        return userExchangeWalletService.getAvailableAmountOfCurrency(currencyCode)
+        return userExchangeWalletService.getCurrencyBalance(currencyCode)
     }
 
     private fun getUserExchangeWalletService(exchangeName: String, exchangeUserId: String): UserExchangeWalletService {
--- autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/wallet/ExchangeWalletService.kt
+++ autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/wallet/ExchangeWalletService.kt
@@ -1,8 +1,9 @@
 package automate.profit.autocoin.exchange.wallet
 
-import java.math.BigDecimal
+import automate.profit.autocoin.exchange.currency.CurrencyBalance
 
 interface ExchangeWalletService {
-    fun getAvailableAmountOfCurrency(exchangeName: String, exchangeUserId: String, currencyCode: String): BigDecimal
+    fun getAvailableAmountOfCurrency(exchangeName: String, exchangeUserId: String, currencyCode: String) = getCurrencyBalance(exchangeName, exchangeUserId, currencyCode).available
+    fun getCurrencyBalance(exchangeName: String, exchangeUserId: String, currencyCode: String): CurrencyBalance
 }
 
