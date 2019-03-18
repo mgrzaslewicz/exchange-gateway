def6cc6175de80f460205ac2ac158a9c7bce899c
Mikołaj Grząślewicz <mikolaj.grzaslewicz@gmail.com>
2019-03-18 22:04:03 +0100

    Add getCurrencyBalancesForEveryExchange to ExchangeWalletService

--- autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/wallet/ExchangeWalletService.kt
+++ autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/wallet/ExchangeWalletService.kt
@@ -2,9 +2,17 @@ package automate.profit.autocoin.exchange.wallet
 
 import automate.profit.autocoin.exchange.currency.CurrencyBalance
 
+data class ExchangeWithErrorMessage(
+        val exchangeName: String,
+        val errorMessage: String?
+) {
+    fun hasNoError() = errorMessage == null
+}
+
 interface ExchangeWalletService {
     fun getAvailableAmountOfCurrency(exchangeName: String, exchangeUserId: String, currencyCode: String) = getCurrencyBalance(exchangeName, exchangeUserId, currencyCode).available
     fun getCurrencyBalance(exchangeName: String, exchangeUserId: String, currencyCode: String): CurrencyBalance
     fun getCurrencyBalances(exchangeName: String, exchangeUserId: String): List<CurrencyBalance>
+    fun getCurrencyBalancesForEveryExchange(exchangeUserId: String): Map<ExchangeWithErrorMessage, List<CurrencyBalance>>
 }
 
