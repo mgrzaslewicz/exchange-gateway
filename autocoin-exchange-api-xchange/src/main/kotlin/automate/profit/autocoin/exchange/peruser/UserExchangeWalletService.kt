a144e9924486f2ecb47e6d016f2d88f7dfd33bde
Mikołaj Grząślewicz <mikolaj.grzaslewicz@gmail.com>
2019-06-29 23:09:15 +0200

    Handle multiple wallets for kucoin

--- autocoin-exchange-api-xchange/src/main/kotlin/automate/profit/autocoin/exchange/peruser/UserExchangeWalletService.kt
+++ autocoin-exchange-api-xchange/src/main/kotlin/automate/profit/autocoin/exchange/peruser/UserExchangeWalletService.kt
@@ -1,9 +1,10 @@
 package automate.profit.autocoin.exchange.peruser
 
-import automate.profit.autocoin.exchange.currency.CurrencyBalance
 import automate.profit.autocoin.exchange.SupportedExchange
+import automate.profit.autocoin.exchange.currency.CurrencyBalance
 import mu.KLogging
 import org.knowm.xchange.currency.Currency
+import org.knowm.xchange.dto.account.Wallet
 import org.knowm.xchange.service.account.AccountService
 import java.math.BigDecimal
 
@@ -19,32 +20,43 @@ open class XchangeUserExchangeWalletService(private val supportedExchange: Suppo
 
     override fun getCurrencyBalance(currencyCode: String): CurrencyBalance {
         logger.info("Requesting currency balance for $supportedExchange-$currencyCode")
-        val accountInfo = wrapped.accountInfo
-
-        val balance = accountInfo.wallet.getBalance(Currency(currencyCode))
+        val wallet = getTradingWallet()
+        val balance = wallet.getBalance(Currency(currencyCode))
         return CurrencyBalance(
                 currencyCode = currencyCode,
-                available = balance.available,
-                frozen = balance.frozen,
-                total = balance.total
+                available = balance?.available ?: BigDecimal.ZERO,
+                frozen = balance?.frozen ?: BigDecimal.ZERO,
+                total = balance?.total ?: BigDecimal.ZERO
         ).also {
             logger.info("Available $currencyCode balance: $it")
         }
     }
 
+    private fun getTradingWallet(): Wallet {
+        val accountInfo = wrapped.accountInfo
+        val wallets = accountInfo.wallets
+        val expectedWalletNameWhenMultipleExist = "trade"
+        return if (wallets.size > 1) {
+            if (!wallets.containsKey(expectedWalletNameWhenMultipleExist)) {
+                logger.warn { "$supportedExchange has multiple wallets, but none named '$expectedWalletNameWhenMultipleExist'" }
+            }
+            wallets.getValue(expectedWalletNameWhenMultipleExist)
+        } else {
+            accountInfo.wallet
+        }
+    }
+
     override fun getCurrencyBalances(): List<CurrencyBalance> {
         logger.info("Requesting currency balances for $supportedExchange")
-        val accountInfo = wrapped.accountInfo
+        val wallet = getTradingWallet()
         val balances = mutableListOf<CurrencyBalance>()
-        accountInfo.wallets.forEach { _, wallet ->
-            wallet.balances.forEach { currency, balance ->
-                balances.add(CurrencyBalance(
-                        currencyCode = currency.currencyCode,
-                        available = balance.available,
-                        frozen = balance.frozen,
-                        total = balance.total
-                ))
-            }
+        wallet.balances.forEach { (currency, balance) ->
+            balances.add(CurrencyBalance(
+                    currencyCode = currency.currencyCode,
+                    available = balance.available,
+                    frozen = balance.frozen,
+                    total = balance.total
+            ))
         }
         logger.info("Requesting currency balances for $supportedExchange done")
         return balances
