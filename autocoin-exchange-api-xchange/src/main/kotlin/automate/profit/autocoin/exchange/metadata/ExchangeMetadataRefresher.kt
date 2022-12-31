55c6a60a2c9749689ad1623940d8400bebb34592
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-12-31 23:29:47 +0100

    Run refreshing metadata in parallel

--- autocoin-exchange-api-xchange/src/main/kotlin/automate/profit/autocoin/exchange/metadata/ExchangeMetadataRefresher.kt
+++ autocoin-exchange-api-xchange/src/main/kotlin/automate/profit/autocoin/exchange/metadata/ExchangeMetadataRefresher.kt
@@ -3,12 +3,15 @@ package automate.profit.autocoin.exchange.metadata
 import automate.profit.autocoin.exchange.SupportedExchange
 import automate.profit.autocoin.exchange.apikey.ServiceApiKeysProvider
 import mu.KLogging
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.TimeUnit
 
 class ExchangeMetadataRefresher(
-        exchangeMetadataFetchers: List<ExchangeMetadataFetcher>,
-        private val exchangeMetadataRepository: FileExchangeMetadataRepository,
-        private val serviceApiKeysProvider: ServiceApiKeysProvider,
-        private val cachingExchangeMetadataService: CachingExchangeMetadataService
+    exchangeMetadataFetchers: List<ExchangeMetadataFetcher>,
+    private val exchangeMetadataRepository: FileExchangeMetadataRepository,
+    private val serviceApiKeysProvider: ServiceApiKeysProvider,
+    private val cachingExchangeMetadataService: CachingExchangeMetadataService,
+    private val executorService: ExecutorService
 ) {
 
     init {
@@ -19,32 +22,38 @@ class ExchangeMetadataRefresher(
 
     companion object : KLogging()
 
-    private val fetchersMap = exchangeMetadataFetchers.map { it.supportedExchange to it }.toMap()
+    private val fetchersMap = exchangeMetadataFetchers.associateBy { it.supportedExchange }
 
     /**
      * Schedule this refresh every x minutes/hours to have fresh metadata
      */
     fun fetchAndSaveFreshMetadataFiles() {
         logger.info { "Refreshing all supported exchange metadata files" }
-        SupportedExchange.values().forEach { supportedExchange ->
-            logger.info { "[$supportedExchange] Refreshing metadata" }
-            try {
-                val apiKeys = serviceApiKeysProvider.getApiKeys(supportedExchange)
-                val freshExchangeMetadata = fetchersMap.getValue(supportedExchange).fetchExchangeMetadata(apiKeys)
-                exchangeMetadataRepository.saveExchangeMetadata(supportedExchange, freshExchangeMetadata)
-                exchangeMetadataRepository.keepLastNBackups(supportedExchange, maxBackups = 100)
-                reinitCache(supportedExchange.exchangeName)
-            } catch (e: Exception) {
-                logger.error(e) { "[$supportedExchange] Exception during metadata refresh" }
-            } catch (e: Throwable/* catch runtime class errors too */) {
-                logger.error(e) { "[$supportedExchange] Error during metadata refresh" }
+        val saveMetadataJobs = SupportedExchange.values().map { supportedExchange ->
+            executorService.submit {
+                logger.info { "[$supportedExchange] Refreshing metadata" }
+                try {
+                    val apiKeys = serviceApiKeysProvider.getApiKeys(supportedExchange)
+                    val freshExchangeMetadata = fetchersMap.getValue(supportedExchange).fetchExchangeMetadata(apiKeys)
+                    exchangeMetadataRepository.saveExchangeMetadata(supportedExchange, freshExchangeMetadata)
+                    exchangeMetadataRepository.keepLastNBackups(supportedExchange, maxBackups = 100)
+                    cachingExchangeMetadataService.refreshMetadata(supportedExchange.exchangeName)
+                    logger.info { "[$supportedExchange] Refreshing metadata finished" }
+                } catch (e: Exception) {
+                    logger.error(e) { "[$supportedExchange] Exception during metadata refresh" }
+                } catch (e: Throwable/* catch runtime class errors too */) {
+                    logger.error(e) { "[$supportedExchange] Error during metadata refresh" }
+                }
             }
         }
-    }
-
-    private fun reinitCache(exchangeName: String) {
-        cachingExchangeMetadataService.removeFromCache(exchangeName)
-        cachingExchangeMetadataService.getMetadata(exchangeName)
+        saveMetadataJobs.forEach {
+            try {
+                it.get(15, TimeUnit.MINUTES)
+            } catch (e: Exception) {
+                logger.error(e) { "Error during waiting for metadata refresh to finish" }
+            }
+        }
+        logger.info { "Refreshing all supported exchange metadata files finished" }
     }
 
 }
