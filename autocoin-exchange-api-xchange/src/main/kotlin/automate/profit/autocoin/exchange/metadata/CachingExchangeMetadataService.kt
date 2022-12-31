573e10ea910c3c2658990728701b68b5d2e60d98
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-12-31 23:29:46 +0100

    Bump kotlin to 1.6.0, coroutines to 1.5.2

--- autocoin-exchange-api-xchange/src/main/kotlin/automate/profit/autocoin/exchange/metadata/CachingExchangeMetadataService.kt
+++ autocoin-exchange-api-xchange/src/main/kotlin/automate/profit/autocoin/exchange/metadata/CachingExchangeMetadataService.kt
@@ -4,17 +4,17 @@ import automate.profit.autocoin.exchange.currency.CurrencyPair
 import java.util.concurrent.ConcurrentHashMap
 
 class CachingExchangeMetadataService(private val decorated: ExchangeMetadataService) : ExchangeMetadataService {
-    private val locks = ConcurrentHashMap<String, Object>()
+    private val locks = ConcurrentHashMap<String, Any>()
     private val cache = ConcurrentHashMap<String, ExchangeMetadata>()
 
     fun removeFromCache(exchangeName: String) {
-        synchronized(locks.computeIfAbsent(exchangeName) { Object() }) {
+        synchronized(locks.computeIfAbsent(exchangeName) { Any() }) {
             cache.remove(exchangeName)
         }
     }
 
     override fun getMetadata(exchangeName: String): ExchangeMetadata {
-        synchronized(locks.computeIfAbsent(exchangeName) { Object() }) {
+        synchronized(locks.computeIfAbsent(exchangeName) { Any() }) {
             return cache.computeIfAbsent(exchangeName) { decorated.getMetadata(exchangeName) }
         }
     }
