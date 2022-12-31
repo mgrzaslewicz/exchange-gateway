d210d4c784fb16747bf0231f9c393bbe1d0142da
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-12-31 23:29:48 +0100

    Cleanup ExchangeMetadataService API and return null when not found - instead of throwing exception

--- autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/metadata/ExchangeMetadataService.kt
+++ autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/metadata/ExchangeMetadataService.kt
@@ -4,6 +4,6 @@ import automate.profit.autocoin.exchange.currency.CurrencyPair
 
 interface ExchangeMetadataService {
     fun getMetadata(exchangeName: String): ExchangeMetadata
-    fun getMetadata(exchangeName: String, currencyPair: CurrencyPair): CurrencyPairMetadata = getMetadata(exchangeName).currencyPairMetadata.getValue(currencyPair)
-    fun getMetadata(exchangeName: String, currency: String): CurrencyMetadata = getMetadata(exchangeName).currencyMetadata.getValue(currency)
+    fun getCurrencyPairMetadata(exchangeName: String, currencyPair: CurrencyPair): CurrencyPairMetadata? = getMetadata(exchangeName).currencyPairMetadata[currencyPair]
+    fun getCurrencyMetadata(exchangeName: String, currency: String): CurrencyMetadata? = getMetadata(exchangeName).currencyMetadata[currency]
 }
