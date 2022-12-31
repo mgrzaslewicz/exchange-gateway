ae281c29e963d65ca3967fc4fb5821e77cf333e0
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-12-31 23:29:37 +0100

    Provider default metadata fetchers for exchanges not fully supported yet

--- autocoin-exchange-api-xchange/src/test/kotlin/automate/profit/autocoin/exchange/metadata/ExchangeMetadataFetcherManualTest.kt
+++ autocoin-exchange-api-xchange/src/test/kotlin/automate/profit/autocoin/exchange/metadata/ExchangeMetadataFetcherManualTest.kt
@@ -1,38 +1,26 @@
 package automate.profit.autocoin.exchange.metadata
 
-import org.junit.After
-import org.junit.Before
 import org.junit.Ignore
 import org.junit.Test
-import org.junit.rules.TemporaryFolder
 
-@Ignore
 class ExchangeMetadataFetcherManualTest {
-    private val tempFolder = TemporaryFolder()
-
-    @Before
-    fun setup() {
-        tempFolder.create()
-    }
-
-    @After
-    fun cleanup() {
-        tempFolder.delete()
-    }
 
     @Test
+    @Ignore
     fun shouldFetchBittrexMetadata() {
         val fetcher = BittrexExchangeMetadataFetcher()
         fetcher.fetchExchangeMetadata()
     }
 
     @Test
+    @Ignore
     fun shouldFetchBinanceMetadata() {
         val fetcher = BinanceExchangeMetadataFetcher()
         fetcher.fetchExchangeMetadata()
     }
 
     @Test
+    @Ignore
     fun shouldFetchKucoinMetadata() {
         val fetcher = KucoinExchangeMetadataFetcher()
         fetcher.fetchExchangeMetadata()
