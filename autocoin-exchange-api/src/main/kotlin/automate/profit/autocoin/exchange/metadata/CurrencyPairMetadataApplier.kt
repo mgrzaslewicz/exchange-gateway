c4ac8660f298d53131a3e371b458570f1df7f2a0
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-12-31 23:29:34 +0100

    Move CurrencyPairMetadataApplier to api

--- autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/metadata/CurrencyPairMetadataApplier.kt
+++ autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/metadata/CurrencyPairMetadataApplier.kt
@@ -3,7 +3,6 @@ package automate.profit.autocoin.exchange.metadata
 import automate.profit.autocoin.exchange.SupportedExchange
 import automate.profit.autocoin.exchange.currency.CurrencyPair
 import mu.KLogging
-import org.knowm.xchange.dto.meta.CurrencyPairMetaData
 import java.math.BigDecimal
 import java.math.BigDecimal.ROUND_DOWN
 import java.math.BigDecimal.ZERO
@@ -18,13 +17,12 @@ class CurrencyPairMetadataApplier {
     /**
      * Returns lowest possible amount for currency pair and price allowed on a given exchange
      */
-    fun getMinimumAmountForPrice(price: BigDecimal, ccyPair: CurrencyPair, metadata: CurrencyPairMetaData, exchange: SupportedExchange): BigDecimal {
+    fun getMinimumAmountForPrice(price: BigDecimal, ccyPair: CurrencyPair, metadata: CurrencyPairMetadata, exchange: SupportedExchange): BigDecimal {
         var minAmount = metadata.minimumAmount?.stripTrailingZeros() ?: ZERO
 
         // This can happen e.g. on Binance where you can have currencies in wallet which are not traded yet and are reported with price 0e-8
         if (price == ZERO)
             minAmount = ZERO
-
         else if (exchange == SupportedExchange.BINANCE) {
             // So called MIN_NOTIONAL condition. This will work only for BTC (0.001) based currency pairs. Would need to adjust to other ccy pairs.
             val minNotionalAmount = BigDecimal(getMinNotionalValue(ccyPair)).divide(price, minAmount.scale(), RoundingMode.UP)
@@ -35,23 +33,23 @@ class CurrencyPairMetadataApplier {
         return minAmount
     }
 
-    fun applyAmountScaleAndLimits(amount: BigDecimal, price: BigDecimal, currencyPair: CurrencyPair, currencyPairMetadata: CurrencyPairMetaData, exchange: SupportedExchange): BigDecimal {
+    fun applyAmountScaleAndLimits(amount: BigDecimal, price: BigDecimal, currencyPair: CurrencyPair, currencyPairMetadata: CurrencyPairMetadata, exchange: SupportedExchange): BigDecimal {
         val minAmount = getMinimumAmountForPrice(price, currencyPair, currencyPairMetadata, exchange).orMin()
         val resultAmount = when {
             amount < minAmount -> ZERO.also { logger.warn("Amount $amount below minimum $minAmount. Cutting it down to 0.") }
-            amount > currencyPairMetadata.maximumAmount.orMax() -> currencyPairMetadata.maximumAmount.also { logger.info("Amount $amount above maximum ${currencyPairMetadata.maximumAmount}. Cutting down to ${currencyPairMetadata.maximumAmount}.") }
+            amount > currencyPairMetadata.maximumAmount.orMax() -> currencyPairMetadata.maximumAmount.orMax().also { logger.info("Amount $amount above maximum ${currencyPairMetadata.maximumAmount}. Cutting down to ${currencyPairMetadata.maximumAmount}.") }
             else -> amount
         }.stripTrailingZeros()
         val scale = when {
-            resultAmount.scale() > currencyPairMetadata.minimumAmount.orMin().stripTrailingZeros().scale() -> currencyPairMetadata.minimumAmount.stripTrailingZeros().scale()
+            resultAmount.scale() > currencyPairMetadata.minimumAmount.orMin().stripTrailingZeros().scale() -> currencyPairMetadata.minimumAmount.orMin().stripTrailingZeros().scale()
             resultAmount.scale() < 0 -> 0 // This one is not a strict requirement, just to avoid numbers like 1e4 instead of 10000 during debugging/logging/testing
             else -> resultAmount.scale()
         }
         return applyScale(resultAmount.stripTrailingZeros(), scale)
     }
 
-    fun applyPriceScale(price: BigDecimal, currencyPairMetadata: CurrencyPairMetaData): BigDecimal {
-        return applyScale(price, currencyPairMetadata.priceScale)
+    fun applyPriceScale(price: BigDecimal, currencyPairMetadata: CurrencyPairMetadata): BigDecimal {
+        return applyScale(price, currencyPairMetadata.scale)
     }
 
     private fun applyScale(value: BigDecimal, scale: Int) = value.setScale(scale, ROUND_DOWN).also { logger.debug("applyScale($value, $scale) = $it") }
