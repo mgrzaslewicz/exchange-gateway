80ef106c80a4fa170ce63e43440af293836af17a
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-12-31 23:29:37 +0100

    Add methods for getting currency pair metadata easier

--- autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/metadata/ExchangeMetadataService.kt
+++ autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/metadata/ExchangeMetadataService.kt
@@ -1,5 +1,8 @@
 package automate.profit.autocoin.exchange.metadata
 
+import automate.profit.autocoin.exchange.currency.CurrencyPair
+
 interface ExchangeMetadataService {
     fun getMetadata(exchangeName: String): ExchangeMetadata
+    fun getMetadata(exchangeName: String, currencyPair: CurrencyPair): CurrencyPairMetadata = getMetadata(exchangeName).currencyPairMetadata.getValue(currencyPair)
 }
