f241477f0bb28e3e286a1a3bdf3836ba057252d3
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-12-31 23:29:42 +0100

    Add comments to SupportedExchange

--- autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/SupportedExchange.kt
+++ autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/SupportedExchange.kt
@@ -4,30 +4,32 @@ package automate.profit.autocoin.exchange
  * After adding new exchange remember to update:
  * SupportedExchange.toXchangeClass
  */
-enum class SupportedExchange(val exchangeName: String) {
+enum class SupportedExchange(val exchangeName: String, val comments: List<String> = emptyList()) {
     BIBOX("bibox"),
     BINANCE("binance"),
     BITBAY("bitbay"),
-    BITFINEX("bitfinex"), // needs update in xchange library to use v2 api
+    BITFINEX("bitfinex", listOf("needs update in xchange library to use v2 api")),
     BITMEX("bitmex"),
-    BITSO("bitso"), // needs update in xchange library to use v3 api
+    BITSO("bitso", listOf("needs update in xchange library to use v3 api")),
     BITSTAMP("bitstamp"),
     BITTREX("bittrex"),
-    BITZ("bitz"), // requires key for reading ticker
+    BITZ("bitz", listOf("requires key for reading ticker")),
     BLEUTRADE("bleutrade"),
     HITBTC("hitbtc"),
-    COINBASEPRO("coinbasepro"),
     CEXIO("cexio"),
-    COINEX("coinex"), // xchange market data service not implemented
+    COINBASEPRO("coinbasepro"),
+    COINBENE("coinbene", listOf("throws 429 error, too frequent requests")),
+    COINDEAL("coindeal", listOf("CoindealMarketDataService.getTicker not implemented yet")),
+    COINEX("coinex", listOf("xchange market data service not implemented")),
     EXMO("exmo"),
     GATEIO("gateio"),
     GEMINI("gemini"),
-    IDEX("idex"), // xchange not adjusted to api changes
+    IDEX("idex", listOf("xchange not adjusted to api changes")),
     KRAKEN("kraken"),
     KUCOIN("kucoin"),
     LUNO("luno"),
     LIVECOIN("livecoin"),
-    POLONIEX("poloniex"),
+    POLONIEX("poloniex", listOf("needs API key for reading ticker")),
     TRADEOGRE("tradeogre"),
     YOBIT("yobit");
 
