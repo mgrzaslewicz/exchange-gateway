7b8d26f7731be01a477dd35eb48333974160aca1
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-12-31 23:29:35 +0100

    Add ExchangeCurrencyPairsInWalletService

--- autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/wallet/ExchangeWalletService.kt
+++ autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/wallet/ExchangeWalletService.kt
@@ -5,5 +5,6 @@ import automate.profit.autocoin.exchange.currency.CurrencyBalance
 interface ExchangeWalletService {
     fun getAvailableAmountOfCurrency(exchangeName: String, exchangeUserId: String, currencyCode: String) = getCurrencyBalance(exchangeName, exchangeUserId, currencyCode).available
     fun getCurrencyBalance(exchangeName: String, exchangeUserId: String, currencyCode: String): CurrencyBalance
+    fun getCurrencyBalances(exchangeName: String, exchangeUserId: String): List<CurrencyBalance>
 }
 
