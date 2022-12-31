013b99ca2f7b0df476541e8cf393de36bace9c7b
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-12-31 23:29:43 +0100

    Add OrderBook.toDto(...)

--- autocoin-exchange-api-rest/src/main/kotlin/automate/profit/autocoin/order/OrderBookResponseDto.kt
+++ autocoin-exchange-api-rest/src/main/kotlin/automate/profit/autocoin/order/OrderBookResponseDto.kt
@@ -1,9 +1,15 @@
 package automate.profit.autocoin.order
 
+import automate.profit.autocoin.exchange.SupportedExchange
+import automate.profit.autocoin.exchange.currency.CurrencyPair
 import automate.profit.autocoin.exchange.orderbook.OrderBook
 
 data class OrderBookResponseDto(
         val exchangeName: String,
+        /**
+         * A/B format
+         */
+        val currencyPair: String,
         val buyOrders: List<OrderBookExchangeOrderDto> = emptyList(),
         val sellOrders: List<OrderBookExchangeOrderDto> = emptyList(),
         val errorMessage: String? = null
@@ -13,3 +19,11 @@ data class OrderBookResponseDto(
             sellOrders = sellOrders.map { it.toOrderBookExchangeOrder() }
     )
 }
+
+fun OrderBook.toDto(exchange: SupportedExchange, currencyPair: CurrencyPair) =
+        OrderBookResponseDto(
+                exchangeName = exchange.exchangeName,
+                currencyPair = currencyPair.toString(),
+                buyOrders = buyOrders.map { it.toOrderBookExchangeOrderDto() },
+                sellOrders = sellOrders.map { it.toOrderBookExchangeOrderDto() }
+        )
