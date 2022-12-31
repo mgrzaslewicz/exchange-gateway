f7fa4788ff34f13947fa9e57b8e5511c5e6a8e27
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-12-31 23:29:51 +0100

    Add get all exchanges metadata method

--- autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/metadata/ExchangeMetadataService.kt
+++ autocoin-exchange-api/src/main/kotlin/automate/profit/autocoin/exchange/metadata/ExchangeMetadataService.kt
@@ -1,8 +1,16 @@
 package automate.profit.autocoin.exchange.metadata
 
+import automate.profit.autocoin.exchange.SupportedExchange
 import automate.profit.autocoin.exchange.currency.CurrencyPair
 
 interface ExchangeMetadataService {
+
+    fun getAllExchangesMetadata(): List<ExchangeMetadata> {
+        return SupportedExchange.values().map {
+            getMetadata(it.exchangeName)
+        }
+    }
+
     fun getMetadata(exchangeName: String): ExchangeMetadata
     fun getCurrencyPairMetadata(exchangeName: String, currencyPair: CurrencyPair): CurrencyPairMetadata? = getMetadata(exchangeName).currencyPairMetadata[currencyPair]
     fun getCurrencyMetadata(exchangeName: String, currency: String): CurrencyMetadata? = getMetadata(exchangeName).currencyMetadata[currency]
