242b41132e842df12bb8de385ff50f72f020ad24
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-12-31 23:29:55 +0100

    Make CachingXchangeProvider computation of cache key injectable
    One libary consumer had already a different way of calculating the key hash

--- exchange-gateway-api-xchange/src/main/kotlin/com/autocoin/exchangegateway/api/exchange/xchange/CachingXchangeProvider.kt
+++ exchange-gateway-api-xchange/src/main/kotlin/com/autocoin/exchangegateway/api/exchange/xchange/CachingXchangeProvider.kt
@@ -2,33 +2,32 @@ package com.autocoin.exchangegateway.api.exchange.xchange
 
 import com.autocoin.exchangegateway.spi.exchange.ExchangeName
 import com.autocoin.exchangegateway.spi.exchange.apikey.ApiKeySupplier
-import org.knowm.xchange.utils.DigestUtils
-import java.security.MessageDigest
+import java.util.concurrent.ConcurrentHashMap
 import org.knowm.xchange.Exchange as XchangeExchange
 
-class CachingXchangeProvider<T>(private val decorated: XchangeProvider<T>) : XchangeProvider<T> {
-    private val cache = mutableMapOf<String, XchangeExchange>()
+interface ApiKeyToCacheKeyProvider<T, K> {
+    operator fun invoke(
+        exchangeName: ExchangeName,
+        apiKey: ApiKeySupplier<T>,
+    ): K
+}
 
-    private fun String?.md5(): String {
-        return if (this == null) "null"
-        else {
-            val md = MessageDigest.getInstance("MD5")
-            DigestUtils.bytesToHex(md.digest(toByteArray()))
-        }
-    }
+/**
+ * @param T the type of the API key ID
+ * @param K the type of cache key for ApiKeySupplier<T>
+ */
+class CachingXchangeProvider<T, K>(
+    private val decorated: XchangeProvider<T>,
+    private val apiKeyToCacheKey: ApiKeyToCacheKeyProvider<T, K>,
+) : XchangeProvider<T> {
+    private val cache = ConcurrentHashMap<K, XchangeExchange>()
 
     override fun invoke(
         exchangeName: ExchangeName,
         apiKey: ApiKeySupplier<T>,
     ): XchangeExchange {
-        val key = apiKey.supplier?.get()
-        val cacheKey = if (key != null) {
-            "$exchangeName:${key.secretKey.md5()}:${key.secretKey.md5()}"
-        }
-        else {
-            exchangeName.value
-        }
-        return cache.getOrPut(cacheKey) {
+        val cacheKey = apiKeyToCacheKey(exchangeName = exchangeName, apiKey = apiKey)
+        return cache.computeIfAbsent(cacheKey) {
             decorated(exchangeName, apiKey)
         }
     }
--- exchange-gateway-api-xchange/src/test/kotlin/com/autocoin/exchangegateway/api/exchange/CachingXchangeProviderTest.kt
+++ exchange-gateway-api-xchange/src/test/kotlin/com/autocoin/exchangegateway/api/exchange/CachingXchangeProviderTest.kt
@@ -1,5 +1,6 @@
 package com.autocoin.exchangegateway.api.exchange
 
+import com.autocoin.exchangegateway.api.exchange.xchange.ApiKeyToCacheKeyProvider
 import com.autocoin.exchangegateway.api.exchange.xchange.CachingXchangeProvider
 import com.autocoin.exchangegateway.api.exchange.xchange.DefaultXchangeProvider
 import com.autocoin.exchangegateway.api.exchange.xchange.ExchangeNames.Companion.binance
@@ -7,15 +8,18 @@ import com.autocoin.exchangegateway.api.exchange.xchange.XchangeApiKeyVerifierGa
 import com.autocoin.exchangegateway.api.exchange.xchange.XchangeInstanceProvider
 import com.autocoin.exchangegateway.api.exchange.xchange.XchangeInstanceWrapper
 import com.autocoin.exchangegateway.api.exchange.xchange.XchangeSpecificationApiKeyAssigner
+import com.autocoin.exchangegateway.spi.exchange.ExchangeName
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.knowm.xchange.Exchange
 import org.knowm.xchange.ExchangeSpecification
+import com.autocoin.exchangegateway.spi.exchange.apikey.ApiKeySupplier as SpiApiKeySupplier
 
 class CachingXchangeProviderTest {
-    private lateinit var tested: CachingXchangeProvider<String>
+    private lateinit var tested: CachingXchangeProvider<String, String>
     private lateinit var countingXchangeInstanceWrapper: CountingXchangeInstanceWrapper
+    private val exchangeName = ExchangeName("exchange1")
 
     @BeforeEach
     fun setup() {
@@ -23,6 +27,14 @@ class CachingXchangeProviderTest {
             decorated = XchangeInstanceWrapper(),
         )
         tested = CachingXchangeProvider(
+            apiKeyToCacheKey = object : ApiKeyToCacheKeyProvider<String, String> {
+                override fun invoke(
+                    exchangeName: ExchangeName,
+                    apiKey: SpiApiKeySupplier<String>,
+                ): String {
+                    return apiKey.id
+                }
+            },
             decorated = DefaultXchangeProvider(
                 xchangeInstanceProvider = countingXchangeInstanceWrapper,
                 xchangeSpecificationApiKeyAssigner = XchangeSpecificationApiKeyAssigner(
@@ -43,10 +55,19 @@ class CachingXchangeProviderTest {
     @Test
     fun shouldUseCachedXchangeInstance() {
         // when
-        tested.invoke(exchangeName = binance, apiKey = com.autocoin.exchangegateway.api.exchange.ApiKeySupplier(id = "1", supplier = null))
-        tested.invoke(exchangeName = binance, apiKey = com.autocoin.exchangegateway.api.exchange.ApiKeySupplier(id = "1", supplier = null))
+        tested.invoke(exchangeName = binance, apiKey = ApiKeySupplier(id = "1", supplier = null))
+        tested.invoke(exchangeName = binance, apiKey = ApiKeySupplier(id = "1", supplier = null))
         // then
         assertThat(countingXchangeInstanceWrapper.invokeCount).isEqualTo(1)
+    }
+
+    @Test
+    fun shouldNotUseCachedXchangeInstance() {
+        // when
+        tested.invoke(exchangeName = binance, apiKey = ApiKeySupplier(id = "1", supplier = null))
+        tested.invoke(exchangeName = binance, apiKey = ApiKeySupplier(id = "2", supplier = null))
+        // then
+        assertThat(countingXchangeInstanceWrapper.invokeCount).isEqualTo(2)
     }
 
 }
